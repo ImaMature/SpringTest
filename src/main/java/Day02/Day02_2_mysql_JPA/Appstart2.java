15b8c24315b16208a4aa19b3c3c75749fde65174
504 <johorea1356@gmail.com>
2021-12-28 15:36:32 +0900

    day2
    JPA로 intelij에서 DB만들어보기 member테이블 생성까지

--- src/main/java/Day02/Day02_2_mysql_JPA/Appstart2.java
+++ src/main/java/Day02/Day02_2_mysql_JPA/Appstart2.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Appstart2 {
     public static void main(String[] args) {
         SpringApplication.run(Appstart2.class,args);
+
     }
 }
